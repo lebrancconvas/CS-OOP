c7ca92409143d245a59a729f91a8b529c6339554
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2020-02-12 11:53:01 +0700

    File Replacement.

---
+++ FileReplacement.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class FileReplacement {
+	public static void main(String[] args) {
+		Scanner fileIn = new Scanner(new FileReader("D:\\Poom\\friend.dat"));
+		String name , tel , address , zipcode;
+		while(fileIn.hasNext()) {
+			new = fileIn.next();
+			fileIn.next();
+			add = fileIn.next() + " " + fileIn.next() + " " + fileIn.next();
+			zipcode = fileIn.next();
+			Scanner file1 = new Scanner(new FileReader("D:\\Poom\\letter.txt"));
+		}
+	}
+}
