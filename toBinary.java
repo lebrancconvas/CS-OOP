a454a54c835616d5c83d9e0a2355421dd1231c2f
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2020-02-19 11:52:06 +0700

    Decimal to Any Base.

--- toBinary.java
+++ toBinary.java
@@ -1,19 +1,31 @@
 //import java.util.*;
 public class toBinary {
 	public static void main(String[] args) {
-		baseToBinary(23);
-		baseToBinary(1000);
-		baseToBinary(8);
+		decimalToBase(23, 2);
+		decimalToBase(5, 2);
 	}
 	
-	public static void baseToBinary(int num) {
-		int b = 0;
+	public static void decimalToBase(int num, int base) {
+//		int b = 0;
+		String bin = "";
 		while(num > 0) {
-			b = (10 * b) + (num % 2);
-			num /= 2;
+			int modu = num % base;
+			num /= base;
+			String modustr = Integer.toString(modu);
+			bin += modustr;
 		}
+		
+		String realconvert = "";
+        
+        
+        for(int i = bin.length() - 1; i >= 0; i--)
+        {
+            realconvert = realconvert + bin.charAt(i);
+        }
+		
+//		bin.replaceFirst("(.)(.*)(.)", "$3$2$1");
 	
-		System.out.println(b);
+		System.out.println(realconvert);
 	}
 }
 	
